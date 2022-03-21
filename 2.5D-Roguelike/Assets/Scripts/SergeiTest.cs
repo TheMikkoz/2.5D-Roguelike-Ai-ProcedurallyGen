4556a8716352eff964cc20eb1b9fa7d143d32898
Merc1073 <sergeikoblov2000@gmail.com>
2022-03-21 01:50:47 +0000

    SergeiTest

--- 2.5D-Roguelike/Assets/Scripts/SergeiTest.cs
+++ 2.5D-Roguelike/Assets/Scripts/SergeiTest.cs
@@ -7,7 +7,7 @@ public class SergeiTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Oh no");
+        Debug.Log("Oh yes");
     }
 
     // Update is called once per frame
