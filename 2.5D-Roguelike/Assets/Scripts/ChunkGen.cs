8a1e4ef22596bc104063d9b6dda2aee9bbbda286
Mikko Jyrkäs <mikko.jyrkas@hotmail.com>
2022-03-21 19:15:07 +0200

    Chunk gen Update

--- 2.5D-Roguelike/Assets/Scripts/ChunkGen.cs
+++ 2.5D-Roguelike/Assets/Scripts/ChunkGen.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ChunkGen : MonoBehaviour
 {
+    [SerializeField] int size;
     [SerializeField] Transform plrPos;
     [SerializeField] private GameObject groundPrefab;
+    [SerializeField] private GameObject WallPrefab;
     List<GameObject> Chunks = new List<GameObject>();
     private Vector3 oldPos = new Vector3(1,0,1);
 
@@ -36,9 +38,10 @@ public class ChunkGen : MonoBehaviour
     {
         if (CheckChunk(Pos))
         {
-            Chunks.Add(Instantiate(groundPrefab, Pos, Quaternion.identity));
+            Chunks.Add(Instantiate(groundPrefab, Pos, Quaternion.identity, this.transform));
         }
     }
+
 
     float Round(float Value)
     {
@@ -52,37 +55,28 @@ public class ChunkGen : MonoBehaviour
         // 0 x + x 0
         // 0 x x x 0
         // 0 0 0 0 0
-        Vector3[] array = new Vector3[8];
-
-        for (int i = 0; i < array.Length; i++)
+        for (int i = (-size * 10); i <= (size * 10); i += 10)
         {
-            CreateChunk(array[i]);
+            for (int ii = (-size * 10); ii <= (size * 10); ii += 10)
+            {
+                CreateChunk(new Vector3(Round(plrPos.position.x) + ii, 0, Round(plrPos.position.z) + i));
+            }
         }
-        CreateChunk(new Vector3(Round(plrPos.position.x) + 10, 0, Round(plrPos.position.z) + 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x), 0, Round(plrPos.position.z) + 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x) - 10, 0, Round(plrPos.position.z) + 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x) - 10, 0, Round(plrPos.position.z)));
-        CreateChunk(new Vector3(Round(plrPos.position.x) - 10, 0, Round(plrPos.position.z) - 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x), 0, Round(plrPos.position.z) - 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x) + 10, 0, Round(plrPos.position.z) - 10));
-        CreateChunk(new Vector3(Round(plrPos.position.x) + 10, 0, Round(plrPos.position.z)));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 20, 0, Round(plrPos.position.z) + 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 10, 0, Round(plrPos.position.z) + 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 20, 0, Round(plrPos.position.z) + 10));
-        deActiveChunk(new Vector3(Round(plrPos.position.x), 0, Round(plrPos.position.z) + 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 20, 0, Round(plrPos.position.z) + 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 20, 0, Round(plrPos.position.z) + 10));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 10, 0, Round(plrPos.position.z) + 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 20, 0, Round(plrPos.position.z)));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 20, 0, Round(plrPos.position.z) - 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 20, 0, Round(plrPos.position.z) - 10));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) - 10, 0, Round(plrPos.position.z) - 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x), 0, Round(plrPos.position.z) - 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 20, 0, Round(plrPos.position.z) - 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 20, 0, Round(plrPos.position.z) - 10));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 10, 0, Round(plrPos.position.z) - 20));
-        deActiveChunk(new Vector3(Round(plrPos.position.x) + 20, 0, Round(plrPos.position.z)));
+        for (int i = (-size * 10 - 10); i <= (size * 10 + 10); i += 10)
+        {
+            deActiveChunk(new Vector3(Round(plrPos.position.x) + (-size * 10 - 10), 0, Round(plrPos.position.z) + i));
+            deActiveChunk(new Vector3(Round(plrPos.position.x) - (-size * 10 - 10), 0, Round(plrPos.position.z) + i));
+            deActiveChunk(new Vector3(Round(plrPos.position.x) + i, 0, Round(plrPos.position.z) + (size * 10 + 10)));
+            deActiveChunk(new Vector3(Round(plrPos.position.x) + i, 0, Round(plrPos.position.z) - (size * 10 + 10)));
+        }
+        /*  20-20  20-10  20-0  20+10  20+20
+         *  10-20  10-10  10-0  10+10  10+20
+         *   0-20   0-10   0-0   0+10  0+20
+         * -10-20 -10-10 -10-0 -10+10 -10+20
+         * -20-20 -20-10 -20-0 -20+10 -20+20
+         */
     }
+
 
     // Start is called before the first frame update
     void Start()
